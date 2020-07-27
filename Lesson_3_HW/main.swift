ac4a0bc21274e1af0d12f8ddff421378441bfede
Feras001 <ferasshaker@mail.ru>
2020-07-27 10:41:11 +0300

    Initial Commit

---
+++ Lesson_3_HW/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson_3_HW
+//
+//  Created by Feras Shaker on 27.07.2020.
+//  Copyright © 2020 Feras Shaker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
